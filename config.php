6f295ef819c51dbc9a144786718c8a05e09a428f
Depayx1 <bangdepp2709@gmail.com>
2025-05-31 18:33:46 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Ganti dengan API downloader yang kamu gunakan
+$api_list = [
+    "youtube" => "https://api.example.com/youtube?url=",
+    "facebook" => "https://api.example.com/facebook?url=",
+    "instagram" => "https://api.example.com/instagram?url=",
+    "tiktok" => "https://api.example.com/tiktok?url="
+];
+?>
